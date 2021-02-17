d66425e033f9013e1721894ddce0401c35e5da2e
Gabriel Nascarella <ielnascarella@hotmail.com>
2021-02-17 10:31:20 -0300

    comeco do path planning

---
+++ ssl-Client/path_planning.cpp
@@ -0,0 +1,45 @@
+#include <math.h>
+
+#define RADIUS 8
+
+typedef struct {
+    double x, y;
+    int radius;
+}  circle_t;
+
+typedef struct {
+    double x, y;
+}  robot_t;
+
+
+int main()
+{
+    int robots_n = 5; // all robots - 1
+
+    circle_t circles[robots_n];
+    robot_t robots[robots_n]; 
+
+    // Generating array of circles
+    for(int i = 0; i < robots_n; i++) {
+        circle_t circle;
+        robot_t robot = robots[i]; // from a list of robots
+
+        circle.x = robot.x;
+        circle.y = robot.y;
+        circle.radius = RADIUS;
+
+        circles[i] = circle;
+    }    
+
+    int circles_n = robots_n;
+
+    // Getting bitangents
+    for (int i = 0; i < circles_n; i++) 
+        for (int j = 0; j < i; j++) {
+            var internal = new InternalBitangents(circles[i], circles[j]);
+            add_edge(i, internal.C, j, internal.F);
+            if (circles[i].r != 0 && circles[j].r != 0) { add_edge(i, internal.D, j, internal.E); }
+            var external = new ExternalBitangents(circles[i], circles[j]);
+            if (circles[i].r != 0 || circles[j].r != 0) { add_edge(i, external.C, j, external.F); }
+            if (circles[i].r != 0 && circles[j].r != 0) { add_edge(i, external.D, j, external.E); }
+}
